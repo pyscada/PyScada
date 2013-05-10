abeb7b8bbaa4335c2b64566f551f8af951d4b92d
Martin Schroeder <schroeder.m@mailbox.tu-berlin.de>
2013-05-10 18:49:00 +0200

    added new functions to utils module

--- scada/utils/__init__.py
+++ scada/utils/__init__.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
+from scada.utils import modbus
+from scada.models import Controllers
+from scada.models import ControllerConfig
+from scada.models import Variables
+from scada.models import InputConfig
 
-
-
-def scale_input(self,Input,scaling):
+def scale_input(Input,scaling):
 		sInput = (float(Input)/float(2**scaling.bit))*(scaling.max_value-scaling.min_value)+scaling.min_value
 		return sInput
+
+def getClientData(id):
+	"""
+	getClientData
+	"""
+	output = {};
+	for entries in ControllerConfig.objects.filter(controllers=id):
+		output[entries.key] = entries.value;
+	return output
+	
+	
+def getActiveClientData():
+	"""
+	getActiveControllerData
+	"""	
+	output = [];
+	for controller in ControllerConfig.objects.all():
+		if Variables.objects.all().filter(active='1',controller=controller.controllers_id).count()!=0:
+			output.append(getClientData(controller.controllers_id))
+	return output
