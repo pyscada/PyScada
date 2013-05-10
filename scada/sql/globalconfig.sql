ec7598c86ceb814aec4e7e2d661647023f0a9f46
Martin Schroeder <schroeder.m@mailbox.tu-berlin.de>
2013-05-10 16:34:00 +0200

    added folder for initial sql data

---
+++ scada/sql/globalconfig.sql
@@ -0,0 +1,5 @@
+INSERT INTO `scada_globalconfig` ( `key`, `value`, `description`)
+VALUES
+	('repeatings','0','record data only an spesified number of times or all the time (0)'),
+	('silentMode','1','print mesages to the console'),
+	('stepsize','5','time betwen the meagurements');
---
+++ scada/sql/messageids.sql
@@ -0,0 +1,3 @@
+INSERT INTO `scada_messageids` (`id`, `level`, `description`)
+VALUES
+	(0,0,'undocumented error');
