a7898e30ea1322377c5cad9c31732152f72cef33
Martin Schroeder <schroeder.m@mailbox.tu-berlin.de>
2013-12-29 20:06:16 +0100

    fixed initial sql data

--- pyscada/sql/globalconfig.sql
+++ pyscada/sql/globalconfig.sql
@@ -1,5 +1,12 @@
-INSERT INTO `FAkS_db`.`pyscada_globalconfig` ( `key`, `value`, `description`)
+LOCK TABLES `pyscada_globalconfig` WRITE;
+/*!40000 ALTER TABLE `pyscada_globalconfig` DISABLE KEYS */;
+
+INSERT INTO `pyscada_globalconfig` (`id`, `key`, `value`, `description`)
 VALUES
-	('repeatings','0','record data only an specified number of times (1..n) or all the time (0)'),
-	('silentMode','1','print messages to the console'),
-	('stepsize','5','time between the measurements in seconds');
+	(1,'repeatings','0','record data only an specified number of times (1..n) or all the time (0)'),
+	(2,'silentMode','1','print messages to the console'),
+	(3,'stepsize','5','time between the measurements in seconds'),
+	(4,'simulation','1','simulate measurement data  ');
+
+/*!40000 ALTER TABLE `pyscada_globalconfig` ENABLE KEYS */;
+UNLOCK TABLES;
--- pyscada/sql/messageids.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `FAkS_db`.`pyscada_messageids` (`id`, `level`, `description`)
-VALUES
-	(0,0,'undocumented error');
