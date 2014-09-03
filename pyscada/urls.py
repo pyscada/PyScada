b9a6da6d34dc0ba84769f4f72f075d3285ac77b1
Martin Schroeder <schroeder.m@mailbox.tu-berlin.de>
2014-09-03 10:22:50 +0200

    added password change page and menu item in front end

--- pyscada/urls.py
+++ pyscada/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     # Public pages
     url(r'^$', 'pyscada.hmi.views.index'),
     url(r'^accounts/logout/$', 'pyscada.hmi.views.logout_view'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',{'template_name': 'login.html'}),
+    url(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change',{'template_name': 'password_change.html'},name='password_change'),
+    url(r'^accounts/password_change_done/$', 'django.contrib.auth.views.password_change_done',{'template_name': 'password_change_done.html'},name='password_change_done'),
+    url(r'^accounts/user_profile_change/$','pyscada.hmi.views.user_profile_change'),
     url(r'^json/cache_data/$', 'pyscada.hmi.views.get_cache_data'),
     url(r'^json/init_data/$', 'pyscada.hmi.views.data'),
     url(r'^json/log_data/$', 'pyscada.hmi.views.log_data'),
