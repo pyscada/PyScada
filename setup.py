9a080a75cdb08eadc218dba58ee191a66f0e8e1f
Martin Schröder <m.schroeder@tu-berlin.de>
2015-06-30 14:29:22 +0200

    updated License and setup.py file

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ import pyscada.core
 
 
 CLASSIFIERS = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
+    'Environment :: Console',
     'Framework :: Django',
     'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: OS Independent',
-    'Programming Language :: Python',
+    'Programming Language :: Python :: 2 :: Only',
+    'Programming Language :: JavaScript',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Scientific/Engineering :: Visualization'
 ]
@@ -21,13 +24,13 @@ setup(
     name='PyScada',
     version=pyscada.core.__version__,
     description='An Python, Django based Open Source Scada System',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    url='http://www.martin-schroeder.net/',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='http://www.martin-schroeder.net/PyScada',
     license='GPL version 3',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
-        'django>=1.6',
+        'django>=1.6,<1.8',
         'numpy>=1.6.0',
         'pillow',
         'python-daemon'
