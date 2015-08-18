adcaac24b9dcb8f2d98eaa54f47f084f4e6e96ec
Martin Schröder <m.schroeder@tu-berlin.de>
2015-08-18 17:31:02 +0200

    added registration as windows service for the WindowsDaemonHandler

--- pyscada/management/commands/PyScadaWindowsDaemonHandler.py
+++ pyscada/management/commands/PyScadaWindowsDaemonHandler.py
@@ -39,3 +39,37 @@ class Command(BaseCommand):
             bt.message = 'done'
             bt.save()
             sys.exit(0)
+
+
+import pythoncom
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+
+
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "PyScada Daemon Handler"
+    _svc_display_name_ = "PyScada Daemon Handler"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        pass
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
