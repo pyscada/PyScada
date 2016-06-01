fe27fa2548e98c5769d2e687a045e3a18d59d1ca
Martin Schroeder <m.schroeder@tu-berlin.de>
2016-06-02 01:04:24 +0200

    add migration for backgrounddaemon restart field

---
+++ pyscada/migrations/0027_auto_20160530_1436.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pyscada', '0026_auto_20160518_0848'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='VariableState',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('pyscada.variable',),
+        ),
+        migrations.AddField(
+            model_name='backgroundtask',
+            name='restart_daemon',
+            field=models.BooleanField(default=False),
+        ),
+    ]
