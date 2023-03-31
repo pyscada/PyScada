836ea765d10bab5cba667e1f350cbe6c96c116b6
Martin Schröder <info@martin-schroeder.net>
2023-03-31 11:51:32 +0200

    added a rudimentary test script
    to test the impact of changes to the pyscada source on the initial
    installation/migration

---
+++ tests/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir test
+cd test
+python3 -m virtualenv ./.env
+source ./.env/bin/activate
+
+pip install ../../
+pip install ../../../PyScada-Modbus/
+pip install ../../../PyScada-Onewire/
+pip install ../../../PyScada-Phant/
+pip install ../../../PyScada-VISA/
+pip install ../../../PyScada-SMBus/
+pip install ../../../PyScada-SystemStat/
+
+django-admin startproject PyScadaServer  --template ./../project_template_with_plugins.zip
+cd PyScadaServer/
+mkdir log
+python3 manage.py migrate
+python3 manage.py collectstatic
+python3 manage.py loaddata color
+python3 manage.py loaddata units
+python3 manage.py pyscada_daemon init
+export DJANGO_SUPERUSER_PASSWORD="test"
+python3 manage.py createsuperuser --username pyscada --email pyscada@pyscada.org --noinput
+python3 manage.py runserver --insecure
+deactivate
+cd ../../
+rm -rf test
