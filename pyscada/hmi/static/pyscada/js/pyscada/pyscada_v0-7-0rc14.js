5e85c4ccba96d534e826cd2ba7e83383efc58de0
clavay <clavayssiere@univ-pau.fr>
2023-11-10 10:52:15 +0100

    version 0.8.1

--- pyscada/hmi/static/pyscada/js/pyscada/pyscada_v0-7-0rc14.js
+++ pyscada/hmi/static/pyscada/js/pyscada/pyscada_v0-7-0rc14.js
@@ -1,6 +1,6 @@
 /* Javascript library for the PyScada web client based on jquery and flot,
 
-version 0.8.0
+version 0.8.1
 
 Copyright (c) 2013-2023 Martin Schröder, Camille Lavayssière
 Licensed under the AGPL.
@@ -21,7 +21,7 @@ Licensed under the AGPL.
  * Script's version
  * @type {string}
  */
- var version = "0.8.0";
+ var version = "0.8.1";
 
  /**
   * Date format : day/month/year hours:minutes:seconds
