2e45a73a1381eb134b98b0ccf27bd8624baff0f3
CL <camillelavayssiere@yahoo.fr>
2024-01-30 15:27:08 +0100

    rewrite pyscada signals
    post_save or pre_delete for Variable, Scaling, DeviceHandler call the
    post_save signal for devices attached.
    
    post_save for device restart the background process of the device or if
    not found, restart the background process of the protocol
    
    pre_delete for device kill the background process of the device

--- pyscada/signals.py
+++ pyscada/signals.py
@@ -26,376 +26,123 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-# @receiver(pre_save, sender=VariableProperty)
-def _vp_value_change(sender, instance, **kwargs):
-    if type(instance) is VariableProperty:
-        if instance.id is not None:  # existing object
-            previous = VariableProperty.objects.get(id=instance.id)
-            if str(instance.last_value) != str(previous.value()):
-                logger.debug(
-                    "VP %s new value = %s - %s"
-                    % (str(instance), instance.last_value, previous.value())
-                )
-                instance.last_value = str(previous.value())
-                instance.value_changed = True
+@receiver(pre_delete, sender=Variable)
+@receiver(post_save, sender=Variable)
+def _variable(sender, instance, **kwargs):
+    """
+    Send signal to restart the bp associated for the device of this variable
+    """
+    if type(instance) is Variable or type(instance) is VariableState:
+        logger.debug(f"post_save or pre_delete {type(instance).__name__}.{instance}-{getattr(instance, 'id', 'new')}")
+        try:
+            post_save.send_robust(sender=type(instance.device), instance=instance.device)
+        except Exception as e:
+            logger.debug(e)
+
+
+@receiver(pre_delete, sender=Scaling)
+@receiver(post_save, sender=Scaling)
+def _scaling(sender, instance, **kwargs):
+    """
+    Send signal to restart the bp associated for each device of a variable using this scaling
+    """
+    if type(instance) is Scaling:
+        logger.debug(f"post_save or pre_delete {type(instance).__name__}.{instance}-{getattr(instance, 'id', 'new')}")
+        try:
+            for variable in instance.variable_set.all():
+                post_save.send_robust(sender=type(variable.device), instance=variable.device)
+        except Exception as e:
+            logger.debug(e)
+
+
+@receiver(pre_delete, sender=DeviceHandler)
+@receiver(post_save, sender=DeviceHandler)
+def _device_handler_post_save(sender, instance, **kwargs):
+    """
+    Send signal to restart the bp associated for each device using this device handler
+    """
+    if type(instance) is DeviceHandler:
+        logger.debug(f"post_save or pre_delete {type(instance).__name__}.{instance}-{getattr(instance, 'id', 'new')}")
+        try:
+            for device in instance.device_set.all():
+                post_save.send_robust(sender=type(device), instance=device)
+        except Exception as e:
+            logger.debug(e)
 
 
 @receiver(post_save, sender=VariableProperty)
-@receiver(post_save, sender=Variable)
+def _vp_post_save(sender, instance, **kwargs):
+    if type(instance) is VariableProperty:
+        # TODO: handle VP change but no value change
+        pass
+
+
 @receiver(post_save, sender=Device)
-@receiver(post_save, sender=Scaling)
-@receiver(post_save, sender=DeviceHandler)
 def _reinit_daq_daemons(sender, instance, **kwargs):
     """
-    update the daq daemon configuration when changes be applied in the models
+    Update the daq daemon configuration when changes be applied in the models
     """
     if type(instance) is Device:
+        logger.debug(f"post_save {type(instance).__name__}.{instance}-{getattr(instance, 'id', 'new')}")
         try:
-            logger.debug(
-                "post_save "
-                + str(type(instance).__name__)
-                + "."
-                + str(instance)
-                + "-"
-                + str(instance.id)
+            bp = BackgroundProcess.objects.get(
+                done=False,
+                failed=False,
+                label=f"pyscada.{instance.protocol.protocol}-{instance.id}",
             )
+        except BackgroundProcess.DoesNotExist:
             try:
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label="pyscada."
-                    + str(instance.protocol.protocol)
-                    + "-"
-                    + str(instance.id),
-                )
-            except BackgroundProcess.DoesNotExist:
                 # for modbus protocol
                 bp = BackgroundProcess.objects.get(
                     done=False,
                     failed=False,
-                    label__startswith="pyscada."
-                    + str(instance.protocol.protocol)
-                    + "-"
-                    + str(instance.id)
-                    + "-",
-                )
-        except Exception as e:
-            # logger.debug("post_save pyscada")
-            logger.debug(e)
-            # new device, add it to the parent process list
-            # todo select only one device not all for that protocol
-            try:
-                if instance.protocol_id == 1:
-                    return False
-                bp = BackgroundProcess.objects.get(pk=instance.protocol_id)
-            except:
-                return False
-        bp.restart()
-    elif type(instance) is Variable or type(instance) is VariableState:
-        try:
-            logger.debug(
-                "post_save "
-                + str(type(instance).__name__)
-                + "."
-                + str(instance.device)
-                + "-"
-                + str(instance.device_id)
-            )
-            try:
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label="pyscada."
-                    + str(instance.device.protocol.protocol)
-                    + "-"
-                    + str(instance.device_id),
+                    label__startswith=f"pyscada.{instance.protocol.protocol}-{instance.id}",
                 )
             except BackgroundProcess.DoesNotExist:
-                # for modbus protocol
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label__startswith="pyscada."
-                    + str(instance.device.protocol.protocol)
-                    + "-"
-                    + str(instance.device_id)
-                    + "-",
-                )
-        except Exception as e:
-            logger.debug(e)
-            # todo select only one device not all for that protocol
-            try:
-                if instance.device.protocol_id == 1:
-                    return False
-                bp = BackgroundProcess.objects.get(pk=instance.device.protocol_id)
-            except:
-                return False
-        bp.restart()
-    elif type(instance) is Scaling:
-        # todo select only one device not all for that protocol
-        logger.debug("post_save " + str(type(instance).__name__) + "." + str(instance))
-        for bp_pk in list(
-            instance.variable_set.all().values_list("device__protocol_id").distinct()
-        ):
-            try:
-                bp = BackgroundProcess.objects.get(pk=bp_pk)
-            except:
-                return False
-            bp.restart()
-    elif type(instance) is DeviceHandler:
-        # todo
-        logger.debug("post_save DeviceHandler from %s" % type(instance))
-        for device in instance.device_set.all():
-            try:
-                logger.debug(
-                    "post_save "
-                    + str(type(instance).__name__)
-                    + "."
-                    + str(instance)
-                    + "-"
-                    + str(instance.id)
-                )
-                try:
-                    bp = BackgroundProcess.objects.get(
-                        done=False,
-                        failed=False,
-                        label="pyscada."
-                        + str(device.protocol.protocol)
-                        + "-"
-                        + str(device.id),
-                    )
-                except BackgroundProcess.DoesNotExist:
-                    # for modbus protocol
-                    bp = BackgroundProcess.objects.get(
-                        done=False,
-                        failed=False,
-                        label__startswith="pyscada."
-                        + str(device.protocol.protocol)
-                        + "-"
-                        + str(device.id)
-                        + "-",
-                    )
-            except Exception as e:
-                # logger.debug("post_save pyscada")
-                logger.debug(e)
                 # new device, add it to the parent process list
                 # todo select only one device not all for that protocol
-                try:
-                    if device.protocol_id == 1:
-                        return False
-                    bp = BackgroundProcess.objects.get(pk=device.protocol_id)
-                except:
+                if instance.protocol_id == 1:
+                    # generic device has no parent process to restart
                     return False
-            bp.restart()
-    elif type(instance) is VariableProperty:
-        return
-        logger.debug("post_save from VP %s" % str(instance))
-        if instance.id is not None:  # existing object
-            if instance.value_changed:
-                instance.value_changed = False
                 try:
-                    logger.debug(
-                        "post_save "
-                        + str(type(instance).__name__)
-                        + "."
-                        + str(instance.variable.device)
-                        + "-"
-                        + str(instance.variable.device_id)
-                    )
-                    try:
-                        bp = BackgroundProcess.objects.get(
-                            done=False,
-                            failed=False,
-                            label="pyscada."
-                            + str(instance.variable.device.protocol.protocol)
-                            + "-"
-                            + str(instance.variable.device_id),
-                        )
-                    except BackgroundProcess.DoesNotExist:
-                        # for modbus protocol
-                        bp = BackgroundProcess.objects.get(
-                            done=False,
-                            failed=False,
-                            label__startswith="pyscada."
-                            + str(instance.variable.device.protocol.protocol)
-                            + "-"
-                            + str(instance.variable.device_id)
-                            + "-",
-                        )
-                except Exception as e:
-                    logger.debug(e)
-                    # todo select only one device not all for that protocol
-                    try:
-                        bp = BackgroundProcess.objects.get(
-                            pk=instance.variable.device.protocol_id
-                        )
-                    except:
-                        return False
-                bp.restart()
-            else:
-                logger.debug("VP something changed (not value)")
+                    bp = BackgroundProcess.objects.get(pk=instance.protocol_id)
+                except BackgroundProcess.DoesNotExist:
+                    logger.debug(f"BackgroundProcess for protocol {instance.protocol_id} not found")
+                    return False
+        except Exception as e:
+            logger.debug(e)
+            return False
+        logger.debug(bp.label)
+        bp.restart()
+        return True
     else:
-        logger.debug("post_save from %s to %s" % (type(sender), type(instance)))
+        logger.debug(f"Unknown post_save from {type(sender)} to {type(instance)}")
 
 
-@receiver(pre_delete, sender=VariableProperty)
-@receiver(pre_delete, sender=Variable)
 @receiver(pre_delete, sender=Device)
-@receiver(pre_delete, sender=Scaling)
-@receiver(pre_delete, sender=DeviceHandler)
 def _del_daq_daemons(sender, instance, **kwargs):
     """
-    update the daq daemon configuration when changes be applied in the models
+    Delete the daq daemon when device is deleted
     """
     if type(instance) is Device:
+        logger.debug(f"pre_delete {type(instance).__name__}.{instance}-{getattr(instance, 'id', 'new')}")
         try:
-            logger.debug(
-                "pre_delete "
-                + str(type(instance).__name__)
-                + "."
-                + str(instance)
-                + "-"
-                + str(instance.id)
+            bp = BackgroundProcess.objects.get(
+                done=False,
+                failed=False,
+                label=f"pyscada.{instance.protocol.protocol}-{instance.id}",
             )
-            try:
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label="pyscada."
-                    + str(instance.protocol.protocol)
-                    + "-"
-                    + str(instance.id),
-                )
-            except BackgroundProcess.DoesNotExist:
-                # for modbus protocol
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label__startswith="pyscada."
-                    + str(instance.protocol.protocol)
-                    + "-"
-                    + str(instance.id)
-                    + "-",
-                )
+        except BackgroundProcess.DoesNotExist:
+            # for modbus protocol
+            bp = BackgroundProcess.objects.get(
+                done=False,
+                failed=False,
+                label__startswith=f"pyscada.{instance.protocol.protocol}-{instance.id}",
+            )
         except Exception as e:
             logger.debug(e)
             return False
         bp.stop(signum=signal.SIGKILL)
-    elif type(instance) is Variable or type(instance) is VariableState:
-        try:
-            logger.debug(
-                "pre_delete "
-                + str(type(instance).__name__)
-                + "."
-                + str(instance.device)
-                + "-"
-                + str(instance.device_id)
-            )
-            try:
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label="pyscada."
-                    + str(instance.device.protocol.protocol)
-                    + "-"
-                    + str(instance.device_id),
-                )
-            except BackgroundProcess.DoesNotExist:
-                # for modbus protocol
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label__startswith="pyscada."
-                    + str(instance.device.protocol.protocol)
-                    + "-"
-                    + str(instance.device_id)
-                    + "-",
-                )
-        except Exception as e:
-            logger.debug(e)
-            return False
-        bp.restart()
-    elif type(instance) is Scaling:
-        # todo select only one device not all for that protocol
-        logger.debug("post_save " + str(type(instance).__name__) + "." + str(instance))
-        for bp_pk in list(
-            instance.variable_set.all().values_list("device__protocol_id").distinct()
-        ):
-            try:
-                bp = BackgroundProcess.objects.get(pk=bp_pk)
-            except:
-                return False
-            bp.restart()
-    elif type(instance) is DeviceHandler:
-        # todo
-        logger.debug("pre_delete DeviceHandler from %s" % type(instance))
-        for device in instance.device_set.all():
-            try:
-                logger.debug(
-                    "pre_delete "
-                    + str(type(device).__name__)
-                    + "."
-                    + str(device)
-                    + "-"
-                    + str(device.id)
-                )
-                try:
-                    bp = BackgroundProcess.objects.get(
-                        done=False,
-                        failed=False,
-                        label="pyscada."
-                        + str(device.protocol.protocol)
-                        + "-"
-                        + str(device.id),
-                    )
-                except BackgroundProcess.DoesNotExist:
-                    # for modbus protocol
-                    bp = BackgroundProcess.objects.get(
-                        done=False,
-                        failed=False,
-                        label__startswith="pyscada."
-                        + str(device.protocol.protocol)
-                        + "-"
-                        + str(device.id)
-                        + "-",
-                    )
-            except Exception as e:
-                logger.debug(e)
-                return False
-            bp.stop(signum=signal.SIGKILL)
-    elif type(instance) is VariableProperty:
-        try:
-            logger.debug(
-                "pre_delete "
-                + str(type(instance).__name__)
-                + "."
-                + str(instance.variable.device)
-                + "-"
-                + str(instance.variable.device_id)
-            )
-            try:
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label="pyscada."
-                    + str(instance.variable.device.protocol.protocol)
-                    + "-"
-                    + str(instance.variable.device_id),
-                )
-            except BackgroundProcess.DoesNotExist:
-                # for modbus protocol
-                bp = BackgroundProcess.objects.get(
-                    done=False,
-                    failed=False,
-                    label__startswith="pyscada."
-                    + str(instance.variable.device.protocol.protocol)
-                    + "-"
-                    + str(instance.variable.device_id)
-                    + "-",
-                )
-        except Exception as e:
-            logger.debug(e)
-            return False
-        bp.restart()
+        return True
     else:
-        logger.debug("pre_delete from %s to %s" % (type(sender), type(instance)))
+        logger.debug(f"Unknown pre_delete from {type(sender)} to {type(instance)}")
