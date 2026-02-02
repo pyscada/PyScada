3aa0d5e02ed0b9b0771142274ade2ad20e936abe
Martin <m.schroeder@tu-berlin.de>
2026-02-02 16:43:59 +0100

    move DjangoDatabase datasource to sub app
    to make it more clear what is the pyscada core, pyscada.djangodatasource
    has to be added to the INSTALLED_APPS list in settings.py

---
+++ pyscada/django_datasource/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyscada import core
+
+__version__ = core.__version__
+__author__ = core.__author__
+__email__ = core.__email__
+__description__ = (
+    "Generic extension for PyScada a Python and Django based Open Source SCADA System"
+)
+__app_name__ = "Generic"
+
+PROTOCOL_ID = 1
