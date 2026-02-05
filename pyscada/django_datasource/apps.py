9c2d098647eb9b3f486d4332d90817f94f67d743
Martin <m.schroeder@tu-berlin.de>
2026-02-05 12:56:12 +0100

    added apps.py for django_datasource
    this is nessesary to have the `default_aut_field` property set for
    exsiting installations

---
+++ pyscada/django_datasource/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class PyScadaDjangoDatasourceConfig(AppConfig):
+    name = "pyscada.django_datasource"
+    verbose_name = _("PyScada Django Datasource")
+    default_auto_field = "django.db.models.AutoField"
