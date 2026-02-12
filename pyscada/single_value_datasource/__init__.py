823642031325f4993638f397257447cf4eb06f9d
Martin <m.schroeder@tu-berlin.de>
2026-02-12 13:02:25 +0100

    added new datasource to store only the most recent value in the database

---
+++ pyscada/single_value_datasource/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyscada import core
+
+__version__ = core.__version__
+__author__ = core.__author__
+__email__ = core.__email__
+__description__ = (
+    "Single Value Datasource for PyScada a Python and Django based Open Source SCADA System"
+)
+__app_name__ = "single_value_datasource"
